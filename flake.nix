27f586af4d5182428f3e7074194ab8243f2a94c7
Elias Ainsworth <pilum-murialis@proton.me>
2024-09-23 08:02:09 -0700

    Inital commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+# flake.nix
+
+{
+  description = "Mamelon Hi-Regular flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.${system}.default = (pkgs.callPackage ./default.nix { });
+    };
+}
